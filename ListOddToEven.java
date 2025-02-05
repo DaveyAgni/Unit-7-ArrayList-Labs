69e1f551448f59e22970a7fbe2820991c40ea0bd
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-05 09:03:08 -0500

    Update ListOddToEven.java

--- ListOddToEven.java
+++ ListOddToEven.java
@@ -9,6 +9,37 @@ public class ListOddToEven
 {
 	public static int go( ArrayList<Integer> ray )
 	{
-		return false;
-	}
+        int odd = -1;
+        int even = -1;
+
+        for (int i = 0; i < ray.size(); i++)
+        {
+            if (ray.get(i) % 2 == 1)
+            {
+                odd = i;
+                break;
+            }
+        }
+
+        if (odd == -1)
+        {
+            return -1;
+        }
+
+        for (int i = odd + 1; i < ray.size(); i++)
+        {
+            if (ray.get(i) % 2 == 0)
+            {
+                even = i;
+                break;
+            }
+        }
+
+        if (even == -1)
+        {
+            return -1;
+        }
+
+        return even - odd;
+    }
 }
