edb0aab39ec0a42866bf1733aa246ff3920a09cb
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-05 09:34:40 -0500

    Update OddToEvenRunner.java

--- OddToEvenRunner.java
+++ OddToEvenRunner.java
@@ -5,9 +5,25 @@
 
 //add imports as needed
 
-public class OddToEvenRunner
+public class ListOddToEvenRunner
 {
 	public static void main( String args[] )
 	{
+        ArrayList<Integer> ray = new ArrayList<>();
+        ray.add(7);
+        ray.add(1);
+        ray.add(5);
+        ray.add(3);
+        ray.add(11);
+        ray.add(5);
+        ray.add(6);
+        ray.add(7);
+        ray.add(8);
+        ray.add(9);
+        ray.add(10);
+        ray.add(12345);
+        ray.add(11);
+
+        System.out.println(ListOddToEven.goList(ray));
 	}
 }
