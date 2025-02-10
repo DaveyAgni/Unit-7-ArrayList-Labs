9a857663f8718e117e1ea9ef4b21785004daa482
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-10 09:44:48 -0500

    Update ListAverage.java

--- ListAverage.java
+++ ListAverage.java
@@ -10,6 +10,31 @@ public class ListAverage
 {
 	public static double go( List<Integer> ray)
 	{
-		return 0;
+        int inital = ray.get(0);
+
+        for(int i = 1; i < ray.size(); i++)
+        {
+            if(ray.get(i) < inital)
+            {
+                inital = ray.get(i);
+            }
+        }
+
+        int smallestInt = inital;
+
+        int inital2 = ray.get(0);
+
+        for(int i = 1; i < ray.size(); i++)
+        {
+            if(ray.get(i) > inital2)
+            {
+                inital2 = ray.get(i);
+            }
+        }
+
+        int LargestInt = inital2;
+
+
+        return((LargestInt + smallestInt) / 2);
 	}
 }
