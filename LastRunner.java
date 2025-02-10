200d9c492434b1719c63f11da411eb29d9b3fb88
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-10 09:25:26 -0500

    Update LastRunner.java

--- LastRunner.java
+++ LastRunner.java
@@ -5,9 +5,21 @@
 
 //add imports as needed
 
+import java.util.ArrayList;
+
 public class LastRunner
 {
 	public static void main( String args[] )
 	{
+		ArrayList<Integer> ray = new ArrayList<Integer>();
+		ray.add(9);
+		ray.add(10);
+		ray.add(-8);
+		ray.add(10000);
+		ray.add(-5000);
+		ray.add(1000);
+
+		System.out.println(ListLast.go(ray));
+
 	}
 }
